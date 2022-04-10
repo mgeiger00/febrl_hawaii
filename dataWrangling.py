ff53b0ef0f1eec31f1d68e588f4aa8a8812d34d4
mgeiger00 <85200957+mgeiger00@users.noreply.github.com>
2022-04-09 16:55:49 -1000

    ensure list of names is unique, remove extra lines when writing

--- dataWrangling.py
+++ dataWrangling.py
@@ -10,22 +10,24 @@ nameList = []
 reader = csv.DictReader(kaggleData)
 
 for row in reader:
-    if row['State'] == 'HI':
+    if row['State'] == 'HI' and row['Name'] not in nameList:
         nameList.append(row['Name'])
 
 kaggleData.close()
 
-df = pd.read_excel('hawaiianGivenname.xlsx')
+df = pd.read_excel('hawaiianGatheredNames.xlsx')
 
 gatheredList = df.values.tolist()
 
-nameList += gatheredList
+for name in gatheredList:
+    if name not in nameList:
+        nameList.append(name)
 columnNames = ['Name', 'freq']
 rowNames = []
 for name in nameList:
     rowNames.append([name, random.randint(1,48)])
 
-with open('hawaiianFirstNames.csv', 'w', encoding="utf-8") as f:
+with open('hawaiianFirstNames.csv', 'w', encoding="utf-8", newline = '') as f:
     write = csv.writer(f) 
     write.writerow(columnNames) 
     write.writerows(rowNames)
