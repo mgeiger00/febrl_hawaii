81036c9a9783037f1207a94b116be18fb3c3566d
mgeiger00 <85200957+mgeiger00@users.noreply.github.com>
2022-04-10 14:52:07 -1000

    ensure unique names, remove addition of frequency

--- dataWrangling.py
+++ dataWrangling.py
@@ -7,25 +7,34 @@ kaggleData = open('StateNames.csv')
 #define list to hold names
 nameList = []
 
+#read in data from kaggle
 reader = csv.DictReader(kaggleData)
 
+#take unique names from the state of HI
 for row in reader:
     if row['State'] == 'HI' and row['Name'] not in nameList:
         nameList.append(row['Name'])
 
 kaggleData.close()
 
+#read in data that teammates gathered
 df = pd.read_excel('hawaiianGatheredNames.xlsx')
 
 gatheredList = df.values.tolist()
 
+#join the names together, ensuring they are unique and also clean the data
 for name in gatheredList:
     if name not in nameList:
         nameList.append(name)
-columnNames = ['Name', 'freq']
+    elif name[0] == '[':
+        name = name[1:-1]
+        nameList.append(name)
+
+#output list of names to csv
+columnNames = ['Name']
 rowNames = []
 for name in nameList:
-    rowNames.append([name, random.randint(1,48)])
+    rowNames.append([name])
 
 with open('hawaiianFirstNames.csv', 'w', encoding="utf-8", newline = '') as f:
     write = csv.writer(f) 
